67d1d5f2946f4e776b2bb6aa29de81ae6250e77a
CalebHur <calebhur@uw.edu>
2023-02-25 21:54:49 -0800

    added comments

--- chart2_example.R
+++ chart2_example.R
@@ -6,7 +6,7 @@ library("ggplot2")
 
 ancient_library_data <- read.csv("~/2013-2023-5-Checkouts-SPL.csv")
 
-# was there ever a surge of interest in EBOOKS?
+# So if there was a surge in e-books, what about books?
 book_library_data <- ancient_library_data %>%
   filter(MaterialType == "BOOK")
 sounddisc_library_data <- ancient_library_data %>%
@@ -14,7 +14,7 @@ sounddisc_library_data <- ancient_library_data %>%
 videodisc_library_data <- ancient_library_data %>%
   filter(MaterialType == "VIDEODISC")
 
-# summarize all checkouts into years
+# Summarize all checkouts into years.
 book_library_data <- book_library_data %>%
   group_by(CheckoutYear, CheckoutMonth) %>%
   summarize(Checkouts = sum(Checkouts, na.rm = FALSE))
@@ -25,17 +25,22 @@ videodisc_library_data <- videodisc_library_data %>%
   group_by(CheckoutYear, CheckoutMonth) %>%
   summarize(Checkouts = sum(Checkouts, na.rm = FALSE))
 
+# Combining into one useful data frame.
 combined_physical_library_data <- 
   left_join(book_library_data, sounddisc_library_data, 
             by = c("CheckoutYear", "CheckoutMonth"))
 combined_physical_library_data <- 
   left_join(combined_physical_library_data, videodisc_library_data, 
             by = c("CheckoutYear", "CheckoutMonth"))
-
 combined_physical_library_data <- combined_physical_library_data %>%
   mutate(time = CheckoutYear + CheckoutMonth / 12)
 
+# Graph.
 ggplot(data = combined_physical_library_data, aes(x = time)) +
-  geom_line(aes(y = Checkouts.x)) +
-  geom_line(aes(y = Checkouts.y)) +
-  geom_line(aes(y = Checkouts))
+  geom_line(aes(y = Checkouts.x, color = "Books"), size = 1) +
+  geom_line(aes(y = Checkouts.y, color = "Sound Discs"), size = 1) +
+  geom_line(aes(y = Checkouts, color = "Video Discs"), size = 1) + 
+  labs(x = "Date (Years)", y = "Daily Checkouts", color = "Material Type") + 
+  scale_color_manual(values = c("skyblue", "darkgreen", "green")) + 
+  scale_x_continuous(limits = c(2013, 2023), breaks = 0:2100) +
+  scale_y_continuous(breaks = seq(0, 300000, 30000))
